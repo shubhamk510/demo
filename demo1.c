f74cf0bb833b6ef134b60cb31a3842515abc8d58
Shubham <shubham@linux-j096.site>
2015-07-29 20:18:02 +0530

    demo

---
+++ demo1.c
@@ -0,0 +1,15 @@
+     #include<stdio.h>
+    int main ()
+    {
+    long int a=0;
+    while(a!=42)
+    {
+    scanf(" %ld", &a);
+    if(a!=42)
+    {
+    printf("%ld\n",a);
+    }
+     
+    }
+    return 0;
+    } 
